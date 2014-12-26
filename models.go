3c8fcb8f2e0631659ade06c242d7b63ae902a20d
Zach Latta <zach@zachlatta.com>
2014-12-25 18:22:02 -0800

    messages: implement model

--- models.go
+++ models.go
@@ -1,6 +1,9 @@
 package parcel
 
-import "encoding/json"
+import (
+	"encoding/json"
+	"time"
+)
 
 // ExchangeMessage represents an individual message for processing by the
 // server or returning to the client.
@@ -202,22 +205,155 @@ type Thread struct {
 	MessagesIDs []string `json:"messageIds"`
 }
 
-// MailMessage represents a mail message. A MailMessage is immutable except for
-// the boolean `isXXX` status properties and the set of mailboxes it is in.
-type MailMessage struct {
+// Emailer represents a sender or recipient of an email.
+type Emailer struct {
+	// Name represents the name of the sender/recipient. If a name cannot be
+	// extracted for an email, this property should be an empty string.
+	Name string `json:"name"`
+
+	// Email is the email address of the sender/recipient.
+	Email string `json:"email"`
+}
+
+// Attachment represents an email attachment.
+type Attachment struct {
+	// ID identifies an attachment. This is only unique when paired with the
+	// message id and has no meaning without reference to that.
+	ID string `json:"id"`
+
+	// URL links to the attachment. The HTTP request must be authenticated.
+	URL string `json:"url"`
+
+	// Type is the Content-Type of the attachment.
+	Type string `json:"type"`
+
+	// Name is the full file name.
+	Name string `json:"name"`
+
+	// Size is the size, in bytes, of the attachment when fully decoded (i.e. the
+	// number of bytes in the file the user would download).
+	Size int64 `json:"size"`
+
+	// IsLine is whether the attachment is referenced by a cid: link from within
+	// the HTML body of the message.
+	IsInline bool `json:"isInline"`
+
+	// Width is the width (in px) of the image, if the attachment is an image.
+	Width int64 `json:"width,omitempty"`
+
+	// Height is the height (in px) of the image, if the attachment is an image.
+	Height int64 `json:"height,omitempty"`
+}
+
+// Message represents an email message. A message is immutable except for the
+// boolean isXXX status properties and the set of mailboxes it is in.
+type Message struct {
 	// A unique, immutable ID that does not change if the message changes
 	// mailboxes.
-	ID string
+	ID string `json:"id"`
 
-	// Has the email not yet been read?
-	IsUnread bool
+	// ThreadID is the id fo the thread to which this message belongs.
+	ThreadID string `json:"threadId"`
 
-	// Has the email been flagged (starred, or pinned)?
-	IsFlagged bool
+	// MailboxIDs are the ids of the mailboxes the message is in. A message must
+	// belong to one or more mailboxes at all times (until it is deleted). This
+	// property is mutable.
+	MailboxIDs []string `json:"mailboxIds"`
 
-	// Is the email a draft?
-	IsDraft bool
+	// InReplyToMessageID is the id of the message this message is a reply to.
+	InReplyToMessageID *string `json:"inReplyToMessageId"`
 
-	// Has the email been answered (replied to)?
-	IsAnswered bool
+	// IsUnread is whether the email not yet been read. This corresponds to the
+	// opposite of the \Seen system flag in IMAP. This property is mutable.
+	IsUnread bool `json:"isUnread"`
+
+	// IsFlagged is whether the email been flagged (starred, or pinned). This
+	// corresponds to the \Flagged system flag in IMAP. This property is mutable.
+	IsFlagged bool `json:"isFlagged"`
+
+	// IsAnswered is whether the email has been replied to. This corresponds to
+	// the \Answered system flag in IMAP. This property is mutable.
+	IsAnswered bool `json:"isAnswered"`
+
+	// IsDraft is whether the email is a draft. This corresponds to the \Draft
+	// system flag in IMAP This property is mutable.
+	IsDraft bool `json:"isDraft"`
+
+	// HasAttachment is whether the message has any attachments.
+	HasAttachment bool `json:"hasAttachment"`
+
+	// RawURL is a url to download the original RFC2822 message from. The HTTP
+	// request must be authenticated.
+	RawURL string `json:"rawUrl"`
+
+	// Headers is a map of the header name to (decoded) header value for all
+	// headers in the message. For headers that occur multiple times (e.g.
+	// Received), the values are concatenated with a single new line (\n)
+	// character in between each one.
+	Headers map[string]string `json:"headers"`
+
+	// From contains the name/email from the parsed From header of the email. If
+	// the email doesn't have a From header, this is nil.
+	From *Emailer `json:"from"`
+
+	// To is a slice of emailers representing the parsed To header of the email,
+	// in the same order as they appear in the header. If the email doesn't have
+	// a To header, this is nil. If the header exists, but does not have any
+	// content, this property should be an empty slice.
+	To *[]Emailer `json:"to"`
+
+	// CC is a slice of emailers representing the parsed Cc header of the email,
+	// in the same order as they appear in the header. If the email doesn't have
+	// a Cc header, this is nil. If the header exists, but does not have any
+	// content, this property should be an empty slice.
+	CC *[]Emailer `json:"cc"`
+
+	// BCC is a slice of emailers representing the parsed Bcc header of the
+	// email. If the email doesn't have a Bcc header (which will be true for most
+	// emails outside of the Sent mailbox), this is nil. If the header exists,
+	// but does not have any content, this property should be an empty slice.
+	BCC *[]Emailer `json:"bcc"`
+
+	// ReplyTo is a emailer from the parsed Reply-To header of the email. If the
+	// email doesn't have a Reply-To header, this is nil.
+	ReplyTo *Emailer `json:"emailer"`
+
+	// Subject is the subject of the message.
+	Subject string `json:"subject"`
+
+	// Date is the date the message was sent (or saved, if the message is a
+	// draft).
+	Date time.Time `json:"date"`
+
+	// Size is the size in bytes of the whole message as counted by the server
+	// towards the user's quota.
+	Size int64 `json:"size"`
+
+	// Preview is intended to be shown as a preview line on a mailbox listing. It
+	// is up to 256 characters of the beginning of a plain text version of the
+	// message body.
+	Preview string `json:"preview"`
+
+	// TextBody is the plain text body part for the message. If there is only an
+	// HTML version of the body, a plain text version will be generated from
+	// this.
+	TextBody *string `json:"textBody"`
+
+	// HTMLBody is the HTML body part for the message, if present. If there is
+	// only a plain text version of the body, an HTML version will be generated
+	// from this. Any scripting content, or references to external plugins, must
+	// be stripped from the HTML by the server.
+	HTMLBody *string `json:"htmlBody"`
+
+	// Attachments is a slice of attachments detailing all the attachments to the
+	// message.
+	Attachments *[]Attachment `json:"attachments"`
+
+	// AttachedMessages is a map from attachment ids to message objects with less
+	// properties and some properties renamed. I'm not really sure what this
+	// means and have asked in the mailing list for clarification.
+	//
+	// See attachedMessages in http://jmap.io/spec.html#messages. Mailing list
+	// discussion: https://groups.google.com/forum/#!msg/jmap-discuss/coEBFDY_A7E/vbIhJFYHCogJ
+	AttachedMessages *map[string]Message `json:"attachedMessages"`
 }
